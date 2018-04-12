ff3599798b7936deb684552baa4760bcea3e9dda
Sheldon Callahan <scallahan@atlassian.com>
2018-04-12 13:15:58 -0700

    update port

--- routes/index.js
+++ routes/index.js
@@ -44,7 +44,7 @@ router.set('view engine', 'hbs')
 // Installation Lifecyle webhook that runs after application is installed
 // -----------------------------------------------------------------------------
 router.post('/installed', function(req, res) {
-   debug('router installed in a conversation')
+  debug('router installed in a conversation')
   const cloudId = req.body.cloudId
   const conversationId = req.body.resourceId
   sendToStride.sendMessage(
@@ -52,7 +52,7 @@ router.post('/installed', function(req, res) {
     conversationId,
     'Hi there! I am all about actions',
     function(err) {
-      if (err)  debug.log(err)
+      if (err) debug.log(err)
     }
   )
   res.sendStatus(204)
@@ -62,7 +62,7 @@ router.post('/installed', function(req, res) {
 // Uninstall Lifecyle webhook that fires after application is uninstalled
 // -----------------------------------------------------------------------------
 router.post('/uninstalled', (req, res) => {
-   debug('App uninstalled from a conversation.')
+  debug('App uninstalled from a conversation.')
   res.sendStatus(204)
 })
 
@@ -74,7 +74,7 @@ router.get('/', function(req, res) {
 })
 
 router.get('/descriptor', (req, res) => {
-   debug('router')
+  debug('router')
   let descriptor = JSON.parse(
     fs.readFileSync(path.join(__dirname, '../app-descriptor.json')).toString()
   )
@@ -100,10 +100,10 @@ router.post('/action-message', jwt.validateJWT, function(
   res
 ) {
   let { actionMessage } = require('../messages/action-message.js')
-   debug(actionMessage)
+  debug(actionMessage)
 
   sendToStride.sendMessage(cloudId, conversation.id, actionMessage, function() {
-     debug('Message Sent: ', actionMessage)
+    debug('Message Sent: ', actionMessage)
     //Stop Webhook from sending 3 times by returning 200;
     res.send('200')
   })
@@ -119,7 +119,7 @@ router.post('/bot-mention', jwt.validateJWT, function(
   let { actionCard } = require('../messages/action-card.js')
 
   sendToStride.sendMessage(cloudId, conversation.id, actionCard, function() {
-     debug('Message Sent: ', actionCard)
+    debug('Message Sent: ', actionCard)
     //Stop Webhook from sending 3 times by returning 200;
     res.send('200')
   })
